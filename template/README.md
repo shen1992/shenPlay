48d1be00c692a81c14e8975e1a52d4aa03c6e657
shen <273073987@qq.com>
2017-03-17 10:11:31 +0800

    增加一个模板

---
+++ template/README.md
@@ -0,0 +1,5 @@
+## 启动方法
+
+- npm i
+- npm start
+- 用浏览器打开localhost:8080
---
+++ template/src/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by shen on 2017/3/16.
+ */
+const hello = () => {
+    console.log('hello webpack!')
+}
+hello()
