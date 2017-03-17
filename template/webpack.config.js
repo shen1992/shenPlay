3ad1debd66ef04ea0feac31350edfd992716de5e
shen <273073987@qq.com>
2017-03-17 10:11:41 +0800

    add

---
+++ template/webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by shen on 2017/3/16.
+ */
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname, // 输出文件的保存路径
+        filename: 'bundle.js' // 输出文件的名称
+    }
+}
